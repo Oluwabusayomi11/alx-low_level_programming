54fa8918ca996837ac140587fa3af2452cfb80b9
Oluwabusayomi Iyekolo <iyekolobusayomi@gmail.com>
2022-09-08 04:14:29 -0700

    Tasks update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the sentence
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
