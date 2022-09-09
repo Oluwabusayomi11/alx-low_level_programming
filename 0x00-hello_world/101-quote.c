d2b59f5e3d4e0cabe754227ed40d1c39c8baacc1
Oluwabusayomi Iyekolo <iyekolobusayomi@gmail.com>
2022-09-09 14:43:33 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 /**
@@ -7,7 +8,9 @@
  */
 int main(void)
 {
+	int s = strlen("and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n"); 
 	write(2, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+			\" - Dora Korpar, 2015-10-19\n", s);
 	return (1);
 }
