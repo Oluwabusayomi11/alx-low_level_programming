e25eb3fab7b5b6af8150e098b569f0a32fc8a13a
Oluwabusayomi Iyekolo <iyekolobusayomi@gmail.com>
2022-09-09 14:18:27 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 
 /**
  * main - prints a phrase followed by a new line 
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	write(2, änd that piece of art is useful\" - Dora Korpar,2015-10-19\n", 59);
+	write(2, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
