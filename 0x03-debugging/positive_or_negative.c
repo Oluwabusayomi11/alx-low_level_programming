a89bca102601d5d2edb2d0dc17e70d27a19079e4
Oluwabusayomi Iyekolo <iyekolobusayomi@gmail.com>
2022-09-14 13:53:51 -0700

    corrected

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,16 +6,13 @@
  * main - assigns a random number to int n everytime
  * @i: variable name
  * it executes, and prints it
- * Return: Always 0 (Success)
  */
 void positive_or_negative(int i)
-
+{
 	if (i > 0)
 		printf("%d is positive\n", i);
 	else if (i == 0)
 		printf("%d is zero\n", i);
 	else
 		printf("%d is negative\n", i);
-
-	return (0);
 }
