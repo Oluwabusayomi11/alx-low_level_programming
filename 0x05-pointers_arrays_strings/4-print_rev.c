3457b1e66b14827256d8d51a1a17077ba9386754
Oluwabusayomi Iyekolo <iyekolobusayomi@gmail.com>
2022-09-20 13:27:19 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len)
+		_putchar(s[--len]);
+
+	_putchar('\n');
+}
