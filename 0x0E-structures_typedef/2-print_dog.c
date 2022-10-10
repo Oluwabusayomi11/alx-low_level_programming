33310e2e6264ea2f95fb7908447d3161d56ecbf9
Oluwabusayomi Iyekolo <iyekolobusayomi@gmail.com>
2022-10-10 09:08:00 -0700

    corrected task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,9 +15,9 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		d->name = "nil";
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner = "nil";
+		d->owner = "(nil)";
 
 	printf("Name: %s \nAge: %.6f \nOwner: %s\n", d->name, d->age, d->owner);
 }
