5f07dab6b084d805ad3a810cc0207de4cd96076e
Oluwabusayomi Iyekolo <iyekolobusayomi@gmail.com>
2022-10-10 09:04:31 -0700

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,23 +17,8 @@ struct dog
 	char *owner;
 };
 
-/**
- * struct dog - a new type
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- * dog_t - Typedef for struct dog
- */
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+
 
 #endif
